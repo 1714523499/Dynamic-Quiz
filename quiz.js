d1bacf61337b714d7b6db64919860e112f28e603
unknown <oilyenko@dio335.dio-soft.local>
2014-12-01 13:51:50 +0200

    Initial commit

---
+++ quiz.js
@@ -0,0 +1,16 @@
+/**
+ * Created by oilyenko on 9/16/2014.
+ */
+var allQuestions = [{question: "Who is Prime Minister of the United Kingdom?",
+                    choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"],
+                    correctAnswer:0},
+                    {question: "Who is the father of Geometry?",
+                    choices: ["Aristotle", "Euclid", "Pythagoras", "Kepler"],
+                    correctAnswer:1}];
+
+$(document).onload(function(){
+
+
+
+
+});
