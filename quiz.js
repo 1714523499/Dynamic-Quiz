bd46e89d0bdc9c5f15949af2f5e842cfb1d11010
ailyenko <aleksey.ilyenko@gmail.com>
2014-12-09 17:46:09 +0200

    Add questions.json file

--- quiz.js
+++ quiz.js
@@ -72,63 +72,80 @@ var goPrevious = (function () {
 })();
 
 var changeHtml = function (question) {
-    $('.question').text(question.question);
-    $('label[for=qa]').html(question.choices[0]);
-    $('label[for=qb]').html(question.choices[1]);
-    $('label[for=qc]').html(question.choices[2]);
-    $('label[for=qd]').html(question.choices[3]);
+    $('.question').fadeOut(function () {
+        $(this).text(question.question)
+    }).add($('input').fadeOut()).add($('label[for=qa]').fadeOut(function () {
+        $(this).html(question.choices[0])
+    })).add($('label[for=qb]').fadeOut(function () {
+        $(this).html(question.choices[1])
+    })).add($('label[for=qc]').fadeOut(function () {
+        $(this).html(question.choices[2])
+    })).add($('label[for=qd]').fadeOut(function () {
+        $(this).html(question.choices[3])
+    })).fadeIn();
+    setTimeout(function () {
+        $('#' + sessionStorage[allQuestions.indexOf(question)]).prop("checked", true);
+    }, 600);
+};
+
+var uncheck = function (checkedBox) {
+    setTimeout(function () {
+        checkedBox.removeAttr('checked');
+    }, 300);
 };
 
 $(document).ready(function () {
+    $(".quiz").hide().delay(400).fadeIn();
     sessionStorage.clear();
     var currentObject = goNext();
     changeHtml(currentObject);
-    var correct = 0;
-    var all = allQuestions.length;
+
     $('#next').on('click', function () {
-        var $checkedBox = $('input[name=q]:radio:checked');
-        if ($checkedBox.length === 0) {
-            alert("Please choose the answer!");
-            return;
-        }
-        sessionStorage[allQuestions.indexOf(currentObject)] = $checkedBox.attr('id');
-        $checkedBox.removeAttr('checked');
-        currentObject = goNext();
-        if (currentObject) {
-            changeHtml(currentObject);
-            $('#' + sessionStorage[allQuestions.indexOf(currentObject)]).prop("checked", true);
-        } else {
-            $('.answers').remove();
-            $('.buttons').remove();
-            for (var j = 0; j < all; j++) {
-                var answer;
-                switch (sessionStorage[j]) {
-                    case "qa":
-                        answer = 0;
-                        break;
-                    case "qb":
-                        answer = 1;
-                        break;
-                    case "qc":
-                        answer = 2;
-                        break;
-                    case "qd":
-                        answer = 3;
-                        break;
-                }
-                if (answer === allQuestions[j].correctAnswer) {
-                    correct++;
-                }
+            var $checkedBox = $('input[name=q]:radio:checked');
+            if ($checkedBox.length === 0) {
+                alert("Please choose the answer!");
+                return;
             }
-            $('.question').text("You did it!").append("<h5>You answered correctly " + correct + " of " + all + " questions</h5>");
+            sessionStorage[allQuestions.indexOf(currentObject)] = $checkedBox.attr('id');
+            uncheck($checkedBox);
+            currentObject = goNext();
+            if (currentObject) {
+                changeHtml(currentObject);
+            } else {
+                $('.answers').add('.buttons').remove();
+                for (var j = 0, all = allQuestions.length, correct = 0; j < all; j++) {
+                    var answer;
+                    switch (sessionStorage[j]) {
+                        case "qa":
+                            answer = 0;
+                            break;
+                        case "qb":
+                            answer = 1;
+                            break;
+                        case "qc":
+                            answer = 2;
+                            break;
+                        case "qd":
+                            answer = 3;
+                            break;
+                    }
+                    if (answer === allQuestions[j].correctAnswer) {
+                        correct++;
+                    }
+                }
+                $('.answers').add('.buttons').fadeOut().remove();
+                $('.question').fadeOut(function () {
+                    $(this).text("You did it!").append("<h5>You have answered correctly " + correct + " of " + all + " questions</h5>")
+                }).fadeIn();
+            }
         }
-    });
+    );
+
     $('#back').on('click', function () {
         var $checkedBox = $('input[name=q]:radio:checked');
         sessionStorage[allQuestions.indexOf(currentObject)] = $checkedBox.attr('id');
-        $checkedBox.removeAttr('checked');
+        uncheck($checkedBox);
         currentObject = goPrevious();
         changeHtml(currentObject);
-        $('#' + sessionStorage[allQuestions.indexOf(currentObject)]).prop("checked", true);
     });
 });
