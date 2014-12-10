7202fb23bd30724736872a0f9b217a74d3669026
ailyenko <aleksey.ilyenko@gmail.com>
2014-12-10 19:45:59 +0200

    Add registration and authentication systems

--- quiz.js
+++ quiz.js
@@ -42,6 +42,10 @@ var uncheck = function (checkedBox) {
     }, 300);
 };
 
+var saveFormState = function (e) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+}
 
 $(document).ready(function () {
     $.ajax({
@@ -54,10 +58,120 @@ $(document).ready(function () {
             allQuestions = data.questions;
         }
     });
-    $(".quiz").hide().delay(400).fadeIn();
     sessionStorage.clear();
-    var currentObject = goNext();
-    changeHtml(currentObject);
+    var currentObject;
+    $('.loginForm').submit(function (e) {
+        var login = $(this).find('input[name=login]').val();
+        if (!login) {
+            alert("You should enter your login first!");
+            saveFormState(e);
+            return false;
+        }
+        var password = $(this).find('input[name=password]').val();
+        if (!password) {
+            alert("You should enter your password first!");
+            saveFormState(e);
+            return false;
+        }
+        if (!localStorage.getItem(login + "_name") || localStorage.getItem(login + "_password") !== password) {
+            alert("Incorrect username or password");
+            saveFormState(e);
+            return false;
+        }
+        var checked = $(this).find('#remember_me').attr("checked");
+        saveFormState(e);
+        currentObject = goNext();
+        var $quiz = $(".quiz");
+        var $greet = $(".greetings");
+        $('.message').append("Hello, " + localStorage.getItem(login + "_name") + "!").append($("<p>")).append("Welcome again!");
+        $(this).fadeOut("slow").replaceWith($greet);
+        $greet.fadeIn().delay(500).fadeOut();
+        changeHtml(currentObject);
+        setTimeout(function () {
+            $quiz.fadeIn("slow");
+        }, 2000);
+    });
+
+    $('#goRegister').on('click', function () {
+        $(this).closest('form').find("input[type=text], textarea").val("");
+        $('.loginForm').hide().delay(100);
+        $('.registrationForm').fadeIn();
+    });
+
+    $('#cancel').on('click', function () {
+        $(this).closest('form').find("input[type=text], textarea").val("");
+        $('.registrationForm').hide().delay(100);
+        $('.loginForm').fadeIn();
+    });
+
+
+    $('.registrationForm').submit(function (e) {
+        var name = $(this).find('input[name=rname]').val();
+        if (!name) {
+            alert("You should enter your name!");
+            saveFormState(e);
+            return false;
+        }
+        var login = $(this).find('input[name=rlogin]').val();
+        if (!login) {
+            alert("You should enter your login!");
+            saveFormState(e);
+            return false;
+        }
+        var password = $(this).find('input[name=rpassword]').val();
+        if (!password) {
+            alert("You should enter your password!");
+            saveFormState(e);
+            return false;
+        }
+        var cpassword = $(this).find('input[name=cpassword]').val();
+        if (!cpassword) {
+            alert("You should enter your password confirmation!");
+            saveFormState(e);
+            return false;
+        }
+
+        if (password !== cpassword) {
+            alert("Your password and password confirmation does not match!");
+            saveFormState(e);
+            return false;
+        }
+
+        if (login.length < 5){
+            alert("You specified too short login! Use at least 5 symbols");
+            saveFormState(e);
+            return false;
+        }
+
+        if (password.length < 5){
+            alert("You specified too short password! Use at least 5 symbols");
+            saveFormState(e);
+            return false;
+        }
+
+        if (localStorage.getItem(login + "_name")) {
+            alert("User with such username already exists! Please choose another one.");
+            saveFormState(e);
+            return false;
+        }
+
+        localStorage.setItem(login + "_password", password);
+        localStorage.setItem(login + "_name", name);
+        saveFormState(e);
+        var $succeed = $(".succeed");
+        $('.message').append("Congratulations, " + name + "!").append($("<p>")).append("You have successfully registered to Quiz!");
+        $(this).fadeOut("slow").replaceWith($succeed);
+        $succeed.fadeIn();
+    });
+
+    $('#goToQuiz').on('click', function () {
+        currentObject = goNext();
+        $(".succeed").fadeOut();
+        changeHtml(currentObject);
+        setTimeout(function () {
+            $(".quiz").fadeIn("slow");
+        }, 800);
+    });
 
     $('#next').on('click', function () {
             var $checkedBox = $('input[name=q]:radio:checked');
@@ -103,8 +217,11 @@ $(document).ready(function () {
     $('#back').on('click', function () {
         var $checkedBox = $('input[name=q]:radio:checked');
         sessionStorage[allQuestions.indexOf(currentObject)] = $checkedBox.attr('id');
-        uncheck($checkedBox);
+        var tmp = currentObject;
         currentObject = goPrevious();
-        changeHtml(currentObject);
+        if (currentObject !== tmp) {
+            uncheck($checkedBox);
+            changeHtml(currentObject);
+        }
     });
 });
